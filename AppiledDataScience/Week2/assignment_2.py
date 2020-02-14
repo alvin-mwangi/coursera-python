2e2e4ae30d17946537a689ff3e901b80857a02d9
alvin-mwangi <alvin.mwangi@gmail.com>
2020-02-13 20:15:20 -0800

    udpated version (group by month and day), plot points by index # (1-365)

--- AppiledDataScience/Week2/assignment_2.py
+++ AppiledDataScience/Week2/assignment_2.py
@@ -14,46 +14,42 @@ def get_weather_data(binsize, hashid):
 
     weather_df = pd.read_csv(file_path)
     # print(weather_df)
-    weather_df_subset = weather_df[(weather_df['Date'] >= '2005-01-01') & (weather_df['Date'] < '2015-01-01')]
+    weather_df_subset = (weather_df[(weather_df['Date'] >= '2005-01-01') & (weather_df['Date'] < '2015-01-01') &
+                                    (weather_df['Date'].str[-5:] != '02-29')  # skip leap day
+                                    ]
+    )
 
-    t_max_data = weather_df_subset[weather_df_subset['Element'] == 'TMAX'][['Date', 'Data_Value']]
-    t_max_grouped = t_max_data.groupby(['Date'])['Data_Value'].max()
-    #     print(t_max_grouped)
+    #     for i in weather_df_subset['Date'].sort_values().unique():
+    #         if("-02-" in i):
+    #             print(i)
+    pd.options.mode.chained_assignment = None  # default='warn'
+    weather_df_subset['MonthDay'] = weather_df_subset['Date'].str[-5:]
 
-    t_min_data = weather_df_subset[weather_df_subset['Element'] == 'TMIN'][['Date', 'Data_Value']]
-    t_min_grouped = t_min_data.groupby(['Date'])['Data_Value'].min()
-    #     print(t_min_grouped)
+    t_max_data = weather_df_subset[weather_df_subset['Element'] == 'TMAX'][['MonthDay', 'Data_Value']]
+    t_max_grouped = t_max_data.groupby(['MonthDay'])['Data_Value'].max()
 
-    #     t_max_data.sort_values(by='Date', inplace=True)
-    #     print(t_max_data.head(10))
-    samplesize = 75
-    t_max_dates = t_max_grouped.index.values
-    t_max_dates = list(map(pd.to_datetime, t_max_dates))  # [:samplesize]
-    t_max_testdata = t_max_grouped.tolist()
-    #     print(t_max_testdata[:5])
-    #     t_max_testdata = t_max_testdata[:samplesize]
+    t_min_data = weather_df_subset[weather_df_subset['Element'] == 'TMIN'][['MonthDay', 'Data_Value']]
+    t_min_grouped = t_min_data.groupby(['MonthDay'])['Data_Value'].min()
 
-    t_min_dates = t_min_grouped.index.values
-    t_min_dates = list(map(pd.to_datetime, t_min_dates))  # [:samplesize]
-    t_min_testdata = t_min_grouped.tolist()
-    #     print(t_max_testdata[:5])
-    #     t_min_testdata = t_min_testdata[:samplesize]
-    print(len(t_min_testdata))
+    t_max_grouped.sort_index(inplace=True)
+    t_min_grouped.sort_index(inplace=True)
 
-    #     print(t_max_dates)
-    #     print(t_max_testdata)
+    print(t_max_grouped.shape)
+    print(t_max_grouped.head())
+
+    print(t_min_grouped.shape)
+    print(t_min_grouped.head())
+
     plt.figure()
     #     plt.plot(t_max_dates, t_max_testdata, 'r-o', t_min_dates, t_min_testdata, 'c-.', markersize=1)
-    plt.plot(t_max_dates, t_max_testdata, '-', color='red')
-    plt.plot(t_min_dates, t_min_testdata, '-', color='blue')
+    plt.plot(t_max_grouped.values, '-', color='red')
+    plt.plot(t_min_grouped.values, '-', color='blue')
 
     #     # fill the area between the linear data and exponential data
-    plt.gca().fill_between(t_max_dates,
-                           t_max_testdata, t_min_testdata,
-                           facecolor='cyan',
+    plt.gca().fill_between(range(1, 366),
+                           t_max_grouped.values, t_min_grouped.values,
+                           #                            facecolor='cyan',
                            alpha=0.35)
-
-    fix_labels()
 
 
 get_weather_data(400, 'fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89')
